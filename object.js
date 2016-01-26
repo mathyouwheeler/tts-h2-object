1f555a7d75d6cbf3dcdfa0268c1eecaf47716740
Matthew <mw@matthewis.me>
2016-01-26 16:13:46 -0500

    init

---
+++ object.js
@@ -0,0 +1,13 @@
+var appData = {
+    name: 'Gmail',
+    mailboxes: [
+        'inbox',
+        'starred',
+        'sent',
+    ],
+    contacts: [
+        {name: 'Shane', lastMessage: "I wont be in class today"},
+        {name: 'Katy', lastMessage: "You're such a nerd"}
+    ]
+
+}
